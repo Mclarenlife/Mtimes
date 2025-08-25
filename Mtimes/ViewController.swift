57804cf6d5db6a508984bd5ca4330f9309c6bfbf
Mclarenlife <mclarenlife@qq.com>
2025-08-26 00:55:35 +0800

    Initial Commit

---
+++ Mtimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mtimes
+//
+//  Created by Mclarenlife on 2025/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MtimesTests/MtimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MtimesTests.swift
+//  MtimesTests
+//
+//  Created by Mclarenlife on 2025/8/26.
+//
+
+import Testing
+@testable import Mtimes
+
+struct MtimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
